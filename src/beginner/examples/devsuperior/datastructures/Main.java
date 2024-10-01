0e7cde89a81a31f2f340584b9e19df0a69b82308
walkerNegrão <walker.esteves.negrao@gmail.com>
2024-10-01 06:00:53 -0300

    Atualização

---
+++ src/beginner/examples/devsuperior/datastructures/Main.java
@@ -0,0 +1,14 @@
+package beginner.examples.devsuperior.datastructures;
+
+public class Main {
+    public static void main(String[] args) {
+        var p1 = new Product("Laptop", 1000.00, 5);
+        var p2 = new Product("Headphones", 200.00, 2);
+
+        System.out.println(p1);
+        System.out.println(p2);
+
+        System.out.println(p1.getName());
+        System.out.println(p2.getName());
+    }
+}
